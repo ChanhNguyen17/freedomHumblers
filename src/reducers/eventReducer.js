804615174452b66e7be80cdd49a108f6916c0b7d
fourtti <fourtti@gmail.com>
2018-11-22 12:51:00 +0200

    comment event reducer line

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -5,7 +5,7 @@ export default function (state = initialState.events, action) {
     switch (action.type) {
         case FETCH_EVENTS:
         	return state.concat(action.payload.data);
-            //return action.payload.data;   
+            //return action.payload.data;
         default:
             return state;
     }
